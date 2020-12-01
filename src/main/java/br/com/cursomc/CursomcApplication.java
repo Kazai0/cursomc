ceb5297ccfc371b6bac742c866114551c979796b
Jean.Franzen <jean.franzen@hotmail.com>
2020-12-01 01:05:25 -0200

    Testando o rest

--- src/main/java/br/com/cursomc/CursomcApplication.java
+++ src/main/java/br/com/cursomc/CursomcApplication.java
@@ -1,13 +1,16 @@
-package com.franzen;
+package br.com.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 
 @SpringBootApplication
 public class CursomcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	
 	}
 
 }
