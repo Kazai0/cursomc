5c3ee8598216f6873a92de02f897a96dab5e5453
Franzen <jean.franzen@hotmail.com>
2020-12-20 12:57:46 -0300

    fazendo um teste

--- src/main/java/br/com/cursomc/CursomcApplication.java
+++ src/main/java/br/com/cursomc/CursomcApplication.java
@@ -43,7 +43,8 @@ public class CursomcApplication implements CommandLineRunner{
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
-	
+	System.out.println("Teste");
+		
 	}
 
 	@Override
@@ -94,7 +95,7 @@ public class CursomcApplication implements CommandLineRunner{
 	enderecoRepository.saveAll(Arrays.asList(e1,e2));
 	
 	
-	
+
 		}
 	
 	
